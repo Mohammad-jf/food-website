afef57c281a09aa0ab1cec85e32fefb159deed1a
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-04-03 19:02:06 +0330

    categories page

---
+++ components/templates/CategoriesPage.jsx
@@ -0,0 +1,12 @@
+
+import styles from './categoriesPage.module.css'
+
+
+
+const CategoriesPage = () => {
+    return (
+        <div>CategoriesPage</div>
+    )
+}
+
+export default CategoriesPage
---
+++ pages/categories/index.jsx
@@ -0,0 +1,20 @@
+import CategoriesPage from "@/components/templates/CategoriesPage"
+
+
+
+
+const Categories = () => {
+    return (
+        <CategoriesPage />
+    )
+}
+
+export default Categories
+
+
+
+
+
+// export async function getServerSideProps() {
+//     return {}
+// }
